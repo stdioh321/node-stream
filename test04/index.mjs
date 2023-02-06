056ef84e949ed8cd5d2166a0730456e467428c76
Henrique <henrique.dias@gruponewway.com.br>
2023-02-05 21:23:24 -0300

    refactor: refactor test04

--- test04/index.mjs
+++ test04/index.mjs
@@ -1,58 +1,66 @@
 // getting-started.js
 import mongoose from 'mongoose'
-import { PassThrough, Transform, Writable } from 'stream';
-import {v4} from 'uuid'
+import { PassThrough, Transform, Writable } from 'stream'
+import { v4 } from 'uuid'
 import { createWriteStream } from 'fs'
 
-
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err))
 
 async function main() {
-  await mongoose.connect(`mongodb://127.0.0.1:30001/test`);
-  const Person = mongoose.model('Person', new mongoose.Schema({ name: {
-    type: String,
-    required: true,
-    minlength: 3
-  } }));
+  await mongoose.connect(`mongodb://127.0.0.1:30001/test`)
+  const Person = mongoose.model(
+    'Person',
+    new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 3,
+      },
+    })
+  )
   // Person.watch()
   //   .on('change', data => console.log({data}));
-try {
+  try {
     await Person.create({
-      name: `Mario-${v4()}`
+      name: `Mario-${v4()}`,
     })
     const cursor = Person.find().cursor()
-  let headers = ['id', 'name']
-  let total = 0
-  cursor
-    .pipe(
-      new Transform({
-        objectMode: true,
-        transform(chunk, encoding, cb){
-          const data = `${chunk._id},${chunk.name}\n`
-          cb(null, data)
-        }
+    let headers = ['id', 'name']
+    let total = 0
+    cursor
+      .pipe(
+        new Transform({
+          objectMode: true,
+          transform(chunk, encoding, cb) {
+            const data = `${chunk._id},${chunk.name}\n`
+            cb(null, data)
+          },
+        })
+      )
+      .pipe(
+        new Transform({
+          objectMode: true,
+          transform(chunk, encoding, cb) {
+            this.counter = this.counter ?? 0
+            if (this.counter > 0) return cb(null, chunk)
+            this.counter++
+            const data = `${headers.join(',')}\n`.concat(`${chunk}`)
+            cb(null, data)
+          },
+        })
+      )
+      .pipe(
+        new PassThrough({
+          transform(chunk, encoding, cb) {
+            total++
+            cb(null, chunk)
+          },
+        })
+      )
+      .on('finish', function (data) {
+        console.log({ total })
       })
-    )
-    .pipe(
-      new Transform({
-        objectMode: true,
-        transform(chunk, encoding, cb){
-          this.counter = this.counter ?? 0
-          if(this.counter > 0) return cb(null, chunk)
-          this.counter++
-          const data = `${headers.join(',')}\n`.concat(`${chunk}`)
-          cb(null, data)
-        }
-      })
-    )
-    .pipe(new PassThrough({transform(chunk, encoding, cb){
-      total++
-      cb(null, chunk)
-    }}))
-    .on('finish', function (data){
-      console.log({total})
-    })
-    .pipe(createWriteStream('out.csv'))
+      .pipe(createWriteStream('out.csv'))
     // .pipe(
     //   new Writable({
     //     objectMode: true,
@@ -62,8 +70,8 @@ try {
     //     }
     //   })
     // )
-    console.log(`Person created`);
+    console.log(`Person created`)
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
